eb73932bc77f4a18d5dd40fb9fd0c826e1e297c3
Evan Pradipta Hardinatha <84654936+kleopasevan@users.noreply.github.com>
2024-03-28 02:45:10 +0700

    Adding Lives Parameter

--- app/src/main/java/com/example/unscramble/ui/GameUiState.kt
+++ app/src/main/java/com/example/unscramble/ui/GameUiState.kt
@@ -21,6 +21,7 @@ package com.example.unscramble.ui
 data class GameUiState(
     val currentScrambledWord: String = "",
     val currentWordCount: Int = 1,
+    val lives: Int = 5, //lives parameter
     val score: Int = 0,
     val isGuessedWordWrong: Boolean = false,
     val isGameOver: Boolean = false
